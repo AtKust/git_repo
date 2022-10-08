5fb9f95c1e61ba4c1541397e13aee83da5058a5d
AtKust <rawia.tahrirs@komar.edu.iq>
2022-10-08 23:43:26 +0300

    save

---
+++ JavaScripts.js
@@ -0,0 +1,62 @@
+let x;
+x = 5;
+const myName ='Jane Austen'; 
+
+let WebProgramming='JavaScript';
+switch(WebProgramming) {
+case "PhP":
+console.log("Server Language!");
+break;
+case "JavaScript":
+console.log("FE and BE web language!");
+break;
+case "HTML":
+console.log("Structure Language!");
+break;
+default:
+console.log("try again");
+break;
+}
+for (i=0;i<5;i++) {
+  console.log("The number is " +i);
+  }
+
+  do {
+    console.log("The number is " +i);
+    i++;
+    }
+    while(i > 5);
+
+//The syntax is:
+
+
+
+//Now let’s look at an example:
+
+let i=0;
+while(i < 5) {
+console.log("The number is " +i);
+i++;
+}
+let shopping=[];
+shopping=['paintBrush','sprayPaint','waterColours','canvas'];
+	
+shopping[0];
+
+function product(a, b) {
+  return a*b;
+  }
+
+  product(8,2);
+
+// The syntax is:
+
+//Now let’s look at an example:
+
+let numbers=[1,2,1,2,3,2,3,1];
+if(numbers[0]==numbers[4] {
+console.log("Correct!");
+}
+else {
+console.log("Wrong, please try again");
+}
